1156d0401d41be9fd9fb10479528cfcd352357af
Youngho Cho <dominoyh5@gmail.com>
2026-03-02 15:39:48 +0900

    fix: quote variables in entrypoint to handle special characters

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,8 +24,8 @@ cat << EOF > /etc/samba/smb.conf
    valid users = $SMB_USERNAME
 EOF
 addgroup -S sambashare
-adduser -D -H -s /sbin/nologin -G sambashare $SMB_USERNAME
-chown $SMB_USERNAME:sambashare /samba/data/
-(echo $SMB_USERPASSWD; echo $SMB_USERPASSWD) | smbpasswd -s -a $SMB_USERNAME
-smbpasswd -e $SMB_USERNAME
+adduser -D -H -s /sbin/nologin -G sambashare "$SMB_USERNAME"
+chown "$SMB_USERNAME":sambashare /samba/data/
+(echo "$SMB_USERPASSWD"; echo "$SMB_USERPASSWD") | smbpasswd -s -a "$SMB_USERNAME"
+smbpasswd -e "$SMB_USERNAME"
 exec smbd --foreground --no-process-group
