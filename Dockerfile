e33a0a6d4166f7dbd60790a356439ad4a622a0ee
Youngho Cho <dominoyh5@gmail.com>
2026-03-02 15:18:13 +0900

    perf: migrate base image from ubuntu to alpine

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,14 @@
-FROM ubuntu:jammy
+FROM alpine:3.21
 
 ENV SMB_USERNAME sambatest
 ENV SMB_USERPASSWD sambatest
 
 VOLUME ["/samba/data"]
-RUN mkdir -p /samba/data
 
-RUN apt update && apt upgrade -y && apt install -y samba
+RUN mkdir -p /samba/data && \
+    apk add --no-cache samba
 
 COPY docker-entrypoint.sh /docker-entrypoint.sh
-
-RUN service nmbd stop
-RUN service smbd stop
 
 EXPOSE 445
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cat << EOF > /etc/samba/smb.conf
 [global]
-   server string = %h server (Samba, Ubuntu)
+   server string = %h server (Samba, Alpine)
    server role = standalone server
    disable netbios = yes
    smb ports = 445
@@ -16,9 +16,9 @@ cat << EOF > /etc/samba/smb.conf
    force directory mode = 2770
    valid users = $SMB_USERNAME
 EOF
-useradd -d /samba/$SMB_USERNAME/ -s /usr/sbin/nologin -g sambashare $SMB_USERNAME
+addgroup -S sambashare
+adduser -D -H -s /sbin/nologin -G sambashare $SMB_USERNAME
 chown $SMB_USERNAME:sambashare /samba/data/
 (echo $SMB_USERPASSWD; echo $SMB_USERPASSWD) | smbpasswd -s -a $SMB_USERNAME
 smbpasswd -e $SMB_USERNAME
-service smbd start
-while true; do sleep 10000; done
+exec smbd --foreground --no-process-group
